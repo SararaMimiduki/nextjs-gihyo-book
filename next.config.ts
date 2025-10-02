043414406009efb5fe49ceb026d6ce6aa0add2d7
村上龍一 <murakami.ryuichi.business@gmail.com>
2025-10-02 18:09:37 +0900

    C13: 6.2.4 開発環境のためのAPIリクエストプロキシを追加

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,22 @@ const nextConfig: NextConfig = {
   compiler: {
     // styledComponentsの有効化
     styledComponents: true
-  }
+  },
   /********* 5.2.2 styled-componentsの設定 ここまで *********/
+
+  /********* 6.2.4 開発環境のためのAPIリクエストプロキシ（rewritesの内容を追加） ここから *********/
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+
+        // ex. http://localhost:8080
+        destination: `${process.env.API_BASE_URL}/:match*`
+      }
+    ]
+  }
+  /********* 6.2.4 開発環境のためのAPIリクエストプロキシ（rewritesの内容を追加） ここまで *********/
 }
 
 /********* 5.2.10 テスト環境構築(本番環境におけるdate-testidの削除) ここから *********/
